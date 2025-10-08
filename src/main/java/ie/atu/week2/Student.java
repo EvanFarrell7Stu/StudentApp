eea3920f57044cd625741382c51d437f97ec6c75
EvanFarrell7Stu <g00326940@atu.ie>
2025-10-08 16:54:09 +0100

    set methods

--- src/main/java/ie/atu/week2/Student.java
+++ src/main/java/ie/atu/week2/Student.java
@@ -1,4 +1,23 @@
 package ie.atu.week2;
 
 public class Student {
+    public String name;
+    public String email;
+    public String StudentId;
+
+    public Student () {
+        System.out.println("");
+        this.name="";
+        this.email="";
+        this.StudentId="";
+
+
+    }
+
+    public void setName(String name) { this.name=name; }
+    public void setEmail(String email) { this.email=email; }
+    public void setStudentId(String StudentId) { this.StudentId=StudentId; }
+
+
+
 }
