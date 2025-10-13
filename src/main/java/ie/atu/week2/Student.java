914642bc688883b4ebb854cc2ce83941162aa83e
Evan Farrell <g00326940@atu.ie>
2025-10-13 20:56:40 +0100

    tidy up and comment

--- src/main/java/ie/atu/week2/Student.java
+++ src/main/java/ie/atu/week2/Student.java
@@ -1,12 +1,19 @@
 package ie.atu.week2;
 
-
-import static com.sun.tools.javac.util.StringUtils.toLowerCase;
-
+//constructor
 public class Student {
-    public String name="none";
-    public String email="none";
+    public String name = "none";
+    public String email = "none";
     public String StudentId;
+
+    public Student() {
+        System.out.println();
+        this.name = "";
+        this.email = "";
+        this.StudentId = "";
+
+
+    }
 
     public String getName() {
         return name;
@@ -32,16 +39,6 @@ public class Student {
     public void setStudentId(String studentId) {
         StudentId = studentId;
     }
-
-    public Student () {
-        System.out.println("");
-        this.name="";
-        this.email="";
-        this.StudentId="";
-
-
-    }
-
 
 
 }
--- src/main/java/ie/atu/week2/StudentApp.java
+++ src/main/java/ie/atu/week2/StudentApp.java
@@ -1,40 +1,40 @@
 package ie.atu.week2;
 
-import ie.atu.week2.Student;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class StudentApp {
     public static void main(String[] args) {
         int count = 0;
-        Scanner sc = new Scanner(System.in);
-        List<Student> studentsList = new ArrayList<Student>();
+        Scanner sc = new Scanner(System.in);//initialises scanner sc
+        List<Student> studentsList = new ArrayList<Student>();//create arraylist called studentlist
         Student student1;
 
-        System.out.println("Please enter the amount of students: ");
+        System.out.println("Please enter the amount of students: ");//get number of entries
         int totalStudents = sc.nextInt();
         sc.nextLine();
-        while (count < totalStudents)
-        {
+        while (count < totalStudents) {
 
             // 1.Prompt for details
             System.out.println("Please enter students name: ");
             String name = sc.nextLine();
             System.out.println("Please enter students email");
             String email = sc.nextLine();
-            for (int i=0;i<studentsList.size();i++) {//indexes array list
+            //1.1 Check for duplicate email
+            for (int i = 0; i < studentsList.size(); i++) {//indexes array list
                 Student studentEntry = studentsList.get(i);//gets student from Arraylist
                 String emailEntry = studentEntry.getEmail();//gets email from studentEntry
-                while(emailEntry.compareTo(email.toLowerCase()) == 0) {//compares to new input
+                while (emailEntry.compareTo(email.toLowerCase()) == 0) {//compares to new input
                     System.out.println("Student " + email + " is already taken");
                     System.out.println("Please enter a different email");
                     email = sc.nextLine();
                 }
             }
             System.out.println("Please enter students id");
-           String studentId = sc.nextLine();
+            String studentId = sc.nextLine();
+
+            //2. Set details
             student1 = new Student();
             student1.setName(name);
             student1.setEmail(email);
@@ -45,12 +45,11 @@ public class StudentApp {
 
         }
 
-
+//3. Print data
         System.out.println("Student list\n3");
-        for(Student student : studentsList) {
-    System.out.println("Name:" + student.getName() + "\tEmail: " + student.getEmail() + "\tStudentId: " + student.getStudentId());
-}
-
+        for (Student student : studentsList) {
+            System.out.println("Name:" + student.getName() + "\tEmail: " + student.getEmail() + "\tStudentId: " + student.getStudentId());
+        }
 
 
     }
